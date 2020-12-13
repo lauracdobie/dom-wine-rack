3b4dd3b8ca9fdd949156c746b072deeb1ed9e7cb
Laura Dobie <moving.toyshop@gmail.com>
2020-12-13 15:31:18 +0000

    Refactored - created a separate createWine function to create a new wine bottle and called it in the addWineToWineRack function.

--- js/app.js
+++ js/app.js
@@ -10,23 +10,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const addWineToWineRack = function(event) {
     event.preventDefault();
-    // console.log('🍷🍷🍷');
-    const name = event.target.name.value
-    const country = event.target.country.value
-    const region = event.target.region.value
-    const type = event.target.type.value
 
-    const newWine = document.createElement('li');
-    newWine.innerHTML = `
-    <h2>${name}</h2>
-    <h3>${country}</h3>
-    <p>${region}</p>
-    <p>${type}</p>
-    `
+    const newWine = createWine(event.target);
+
     const wineRack = document.querySelector('ul');
     wineRack.appendChild(newWine);
-    document.getElementById('add-new-wine').reset();
+
+    event.target.reset();
 };
+
+const createWine = function (form) {
+    const name = form.name.value;
+    const country = form.country.value;
+    const region = form.region.value;
+    const type = form.type.value;
+
+    const newWine = document.createElement('li');
+    newWine.classList.add('wine');
+
+    const wineName = document.createElement('h2');
+    wineName.textContent = name;
+    newWine.append(wineName);
+
+    const wineCountry = document.createElement('h3');
+    wineCountry.textContent = country;
+    newWine.append(wineCountry);
+
+    const wineRegion = document.createElement('p');
+    wineRegion.textContent = region;
+    newWine.append(wineRegion);
+
+    const wineType = document.createElement('p');
+    wineType.textContent = type;
+    newWine.appendChild(wineType);
+
+    return newWine;
+}
 
 const drinkAllTheWine = function (event) {
     const rackContents = document.querySelector('ul');
